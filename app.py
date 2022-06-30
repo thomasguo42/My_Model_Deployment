4c8362edb39064e62c925a21a132d758eff50635
Jeff Shang <jingsun_us@yahoo.com>
2022-06-29 22:08:14 -0400

    Update app.py

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ def home():
 def ValuePredictor(to_predict_list):
     to_predict = np.array(to_predict_list).reshape(1, 12)
     loaded_model = pickle.load(open("adult_flask.pkl", "rb"))
+    print("start prediction")
     result = loaded_model.predict(to_predict)
     return result[0]
  
@@ -22,9 +23,11 @@ def ValuePredictor(to_predict_list):
 def result():
     if request.method == 'POST':
         to_predict_list = request.form.to_dict()
+        print("request.form.to_dict(): ", to_predict_list)
         to_predict_list = list(to_predict_list.values())
         to_predict_list = list(map(int, to_predict_list))
-        result = ValuePredictor(to_predict_list)       
+        result = ValuePredictor(to_predict_list)  
+        print("got result: ", result);
         if int(result)== 1:
             prediction ='Income more than 50K'
         else:
