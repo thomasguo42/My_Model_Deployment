0c6d16a9f32a2bb2ce2e3aa5d979bf19359a177a
thomasguo42 <79431394+thomasguo42@users.noreply.github.com>
2022-07-11 14:01:05 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ import numpy as np
 import pandas as pd
 import pickle
 from flask import Flask, request, render_template
+import detectron2
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
+    print('this is home')
     return render_template('index.html')
 
 
